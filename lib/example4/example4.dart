1790188bd2ba5b3bbd484f8b613055c1d32d7fb8
kinleyrabgay <fsd.rabgay@gmail.com>
2024-09-11 21:19:57 +0600

    app: animated container
    Signed-off-by: kinleyrabgay <fsd.rabgay@gmail.com>

---
+++ lib/example4/example4.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class Example4 extends StatefulWidget {
+  const Example4({super.key});
+
+  @override
+  State<Example4> createState() => _Example4State();
+}
+
+const defaultWidth = 100.0;
+
+class _Example4State extends State<Example4> {
+  var _isZoomedIn = false;
+  var _buttonTitle = 'Zoom In';
+  var _width = defaultWidth;
+  var _curve =
+      Curves.easeInOut; // Changed the curve to easeInOut for a smooth effect
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedContainer(
+              duration: const Duration(milliseconds: 370),
+              width: _width,
+              curve: _curve,
+              child: Image.network(
+                'https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png',
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  _isZoomedIn = !_isZoomedIn;
+                  _buttonTitle = _isZoomedIn ? 'Zoom Out' : 'Zoom In';
+                  _width = _isZoomedIn
+                      ? MediaQuery.of(context).size.width
+                      : defaultWidth;
+
+                  _curve = _isZoomedIn
+                      ? Curves.easeInOut
+                      : Curves
+                          .easeInOut; // Using the same smooth curve for both states
+                });
+              },
+              child: Text(_buttonTitle),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animation_course/example3/example3.dart';
+import 'package:flutter_animation_course/example4/example4.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Example3(),
+      home: const Example4(),
     );
   }
 }
